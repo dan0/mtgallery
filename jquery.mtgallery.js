c6aa0d8b48ec4528d410f5684de98985dc06f758
Dan <dan@allbutlost.com>
2011-07-24 21:10:14 +0100

    loading thumbs, resizing based on max thumb height/width

--- jquery.mtgallery.js
+++ jquery.mtgallery.js
@@ -22,7 +22,12 @@ jQuery.fn.mtgallery = function(options) {
         var slides = [];
         
         // wrap the element in a mtgallery div
-        $el.wrap('<div class="mtgallery"/>');
+        $el.wrap($('<div class="mt-gallery"/>'));
+        var $gal = $el.parent('.mt-gallery');
+        $gal.append('<div class="mt-viewer"></div><ul class="mt-thumbs"></ul>');
+        var $viewer = $gal.find('.mt-viewer');
+        var $thumbs = $gal.find('.mt-thumbs');
+        
         
         function embedYouTube (id) {
             var html = '';
@@ -52,19 +57,62 @@ jQuery.fn.mtgallery = function(options) {
             
         }
         
+        function loadImg ($img, order) {
+            
+            var $thumb = $img.clone();
+            
+            
+            $thumbs.children().eq(order).append($thumb);
+            
+            var width = $thumb.width();
+            var height = $thumb.height();
+            
+            if (width > options.thumbMaxWidth) {
+                ratio = options.thumbMaxWidth / width; 
+                $thumb.css("width", options.thumbMaxWidth); // Set new width
+                $thumb.css("height", height * ratio);  // Scale height based on ratio
+                width = options.thumbMaxWidth;
+                height = height * ratio;    // Reset height to match scaled image
+            };
+            if(height > options.thumbMaxHeight){
+                ratio = options.thumbMaxHeight / height; 
+                $thumb.css("height", options.thumbMaxHeight);   
+                $thumb.css("width", width * ratio);  
+                width = width * ratio;
+            }
+            
+        }
+        
         // go through each child of selector and
         // pick out links etc
         $el.children().each(function(i) {
+            
            var $this = $(this);
+           
            // get only the first link
            var $link = $this.find('a:first');
-           var ytid = getId($link.attr('href'));
+           var href = $link.attr('href');
+           var ytid = getId(href);
+           
+           $viewer.append('<div/>');
+           $thumbs.append('<li/>');
            
            if (ytid) {
-               parseYtLink(ytid,i);
+               
+               var thumbUrl = "http://img.youtube.com/vi/" + ytid + "/0.jpg";
+
            }
            else {
-               parseImgLink($link.attr('href'));
+               // Is an image link
+               //parseImgLink($link.attr('href'), );
+               
+               var $img = $("<img/>") 
+                   .attr("src", href)
+                   .load(function() {
+                      loadImg($(this), i);
+                   });
+               
+               
            }
            
         });
